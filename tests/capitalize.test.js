abc5fcb30942111eeb57c2facf64492930e5e71a
Yan Shabalin <yanshabalin@gmail.com>
2023-03-04 23:21:05 +0300

    assert be like

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,7 @@
 import { capitalize } from "../src/capitalize.js";
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-}
-  
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
   
 console.log('Все тесты пройдены!');
