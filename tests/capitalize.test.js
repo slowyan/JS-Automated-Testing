3b663d49f57ba7eeb46c785126ae9eaff31ff598
Yan Shabalin <yanshabalin@gmail.com>
2023-03-12 00:32:19 +0300

    initial

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -3,5 +3,5 @@ import { strict as assert } from 'node:assert';
 
 assert.equal(capitalize('hello'), 'Hello');
 assert.equal(capitalize(''), '');
-  
+assert.equal(capitalize('h'), 'H')
 console.log('Все тесты пройдены!');
